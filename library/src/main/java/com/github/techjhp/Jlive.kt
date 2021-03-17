3eabf563679f6635d6bd745e841b2c8cc3b68f17
pjh <pjh@datau.co.kr>
2021-03-17 12:20:40 +0900

    Changing duplicated dependencies

--- library/src/main/java/com/github/techjhp/Jlive.kt
+++ library/src/main/java/com/github/techjhp/Jlive.kt
@@ -5,5 +5,6 @@ import android.util.Log
 class Jlive {
     companion object {
         fun showJliveLog() = Log.d("J-live", "log")
+        fun showJliveWarnLog() = Log.w("J-live", "warn-log")
     }
 }
